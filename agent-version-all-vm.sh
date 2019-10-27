36bcaafc9b1c1898e8f48ff9e4063e3beee76ac5
Toshikazu Aoyama <toaoyama@microsoft.com>
2019-10-27 12:45:40 +0000

    Update agent-version-all-vm.sh

--- agent-version-all-vm.sh
+++ agent-version-all-vm.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-az vm get-instance-view --ids $(az vm list --query '[].id' -o tsv) | \
-	jq '.[] | {ResourceGroup: .resourceGroup, VMName: .name, OS: .instanceView.osName, OSversion: .instanceView.osVersion, AgentVersion: .instanceView.vmAgent.vmAgentVersion}'
+az vm get-instance-view --ids $(az vm list --query '[].id' -o tsv) --query '[].{ResourceGroup:resourceGroup, VMName:name, OS: instanceView.osName, OSversion: instanceView.osVersion, AgentVersion: instanceView.vmAgent.vmAgentVersion}' -o table
